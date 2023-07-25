abff1555cde0088ecf5b13a693d21adf02ce72ed
S09Z <S09Z@users.noreply.github.com>
2023-07-25 14:41:49 +0000

    deploy: 400ed00620fcaf343a7c88851552ab466035d991

--- _next/static/1k20DBBtHDRf5huQg8bC3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/965-b0299fdc19fde350.js","static/chunks/pages/index-cf24ec9aad73c78b.js"],"/_error":["static/chunks/pages/_error-4a2ede06ef113480.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
