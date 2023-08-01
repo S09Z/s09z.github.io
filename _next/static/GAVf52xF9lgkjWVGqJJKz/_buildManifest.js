4520c90966d8bcdc76118a888ef64ff42dfca24e
S09Z <S09Z@users.noreply.github.com>
2023-08-01 08:07:04 +0000

    deploy: 9f05f1ab7d0627d290334d01791be3956cdd5551

--- _next/static/GAVf52xF9lgkjWVGqJJKz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/148-1fc839c614e9d341.js","static/chunks/pages/index-f2d46e2a33d3549e.js"],"/_error":["static/chunks/pages/_error-4a2ede06ef113480.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
